6b9c4dc03e3a9c45e652f59b4307ec0d0ac09979
rmar72 <rsolm72@gmail.com>
2025-01-26 22:39:35 -0700

    removing unused ref

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['s3-alpha-sig.figma.com', 'cdn-icons-png.flaticon.com', 'icons.veryicon.com', 'static-00.iconduck.com'],
+    domains: ['s3-alpha-sig.figma.com', 'cdn-icons-png.flaticon.com', 'static-00.iconduck.com'],
   },
 
 };
